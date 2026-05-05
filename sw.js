bd3dd96c95fb0e4a4ccfe1850fc5c61ee27f84ce
mohammedfarooqkareemuddin-eng <mohammedfarooqkareemuddin@gmail.com>
2026-05-05 21:23:10 +0530

    Add service worker for caching resources

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'resumerocket-v2';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/resume-builder.html',
+  '/manifest.json',
+  '/icon0.svg',
+  '/icon1.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(name) {
+          return name !== CACHE_NAME;
+        }).map(function(name) {
+          return caches.delete(name);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
